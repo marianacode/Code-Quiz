68b81b29baa2d9ed5f8f8de8e3ac3d55e721c38a
Mariana Robertson <marygmaciel@gmail.com>
2020-07-14 20:18:58 -0700

    Homework 4

---
+++ script.js
@@ -0,0 +1,18 @@
+// arrays
+
+var questions = [{
+    question: "Inside which HTML element do we put the JavaScript?", 
+    choices: ["<javascript>( )", "<script>( )", "<js>( )", "<scripting>( )" ],
+    answer: "<script>( )"
+},
+{ 
+    question: "Where is the correct place to insert a JavaScript?",
+    choices: ["Both the <head> section and the <body> section( )", "The <body> section( )", "The <head> section( )", "Any place( )"],
+    answer: "Both the <head> section and the <body> section( )"
+
+},
+{
+    question: "How do you write 'Hello World' in an alert box?", 
+    choices: ["msg('Hello World');( )", "msgBox('Hello World');( )", "alertBox('Hello World';)( )", "alert('Hello World');( )"],
+    answer: "alert('Hello World')( );"
+}
