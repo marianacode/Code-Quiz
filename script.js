397595724c10964cac1513bf68b454681e9f26d6
Mariana Robertson <marygmaciel@gmail.com>
2020-07-20 16:33:34 -0700

    Homework-4

--- script.js
+++ script.js
@@ -39,11 +39,11 @@ var timer;
 // function 
 
 function start() {
-    timeLeft = 75;
+    timeLeft = 60;
     document.getElementById("timeLeft").innerHTML = timeLeft;
 
 
-    timer = setInterval(function() {
+    timer = setInterval(function () {
         timeLeft--;
         document.getElementById("timeLeft").innerHTML = timeLeft;
 
@@ -52,7 +52,7 @@ function start() {
             endGame();
         }
     }, 1000);
-    
+
     next();
 }
 
@@ -60,13 +60,92 @@ function endGame() {
     clearInterval(timer);
 
     var quizContent = `
-    <>Game Over!</>
-    <>You got ` + score / 20 + ` questions correct!</>
-    <>Your score is ` + score + ` /100</>
+    <h2>Game Over!</h2>
+    <h3>You got ` + score / 20 + ` questions correct!</h3>
+    <h3>Your score is ` + score + ` /100<h3/>
     <input type="text" id="name" placeholder="First name">
-    <button onclick="setScore()">Set score!</button>`;
+    <button onClick="setScore()">Set score!</button>`;
 
     document.getElementById("quizBody").innerHTML = quizContent;
-    
+
 
 }
+
+function setScore() {
+    localStorage.setItem("highscore", score);
+    localStorage.setItem("highscoreName", document.getElementById('name').value);
+    getScore();
+}
+
+function getScore() {
+    var quizContent = `
+    <h1>` + localStorage.getItem("highscore") + `</h1>
+    <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+
+    <button onClick="clearScore()">Clear Score!</button><button onClick="resetGame()">Play Again!</button>
+`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+
+}
+
+function clearScore() {
+    localStorage.setItem("highscore", "");
+    localStorage.setItem("highscoreName", "");
+    resetGame();
+
+}
+
+function resetGame() {
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0;
+    timer = null;
+
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    var quizContent = `
+    <h1>JavaScript Quiz!<h1>
+    <h2>Click to Play!<h2>
+    <button onClick="Start()"<button/>`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+
+}
+
+
+function incorrect() {
+    timeLeft -= 10;
+    next();
+
+}
+
+function next() {
+    currentQuestion++;
+
+    if (currentQuestion > questions.length -1) { 
+        endGame();
+        return;
+
+    }
+
+    var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
+
+    for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
+        var buttonCode = "<button on click=\"[ANS]\">[CHOICE]</button";
+        buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestions].choices[buttonLoop]);
+        if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
+            buttonCode = buttonCode.replace("[ANS}", "correct()");
+        } else {
+            buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+
+        }
+        quizContent += buttonCode
+        }
+
+        document.getElementById("quizBody").innerHTML = quizContent;
+    }
+
+
+
